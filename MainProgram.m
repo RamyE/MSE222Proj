5e3f37081c214f9991da8a39b916d3fb14561293
Ramy ElMallah <ramyelmallah@gmail.com>
2018-03-24 11:15:06 -0700

    Add files via upload

---
+++ MainProgram.m
@@ -0,0 +1,13 @@
+% I am assuming standard metric units (metre, kg, second, N, etc...)
+springK = 5;  %I don't what is its range or expected value
+springS1 = 0.02;
+springS2 = 0.035; %Assumed values given that S2 has to be larger than S1 (After spring is released)
+%S2 is also considered the spring equilibrium position
+mass = 0.05;
+
+initialV = spring (springK, springS1, springS2, mass);
+%the initial velocity in the X direction.
+% we will start our initial X after the spring distance
+
+
+%new comment
---
+++ spring.m
@@ -0,0 +1,6 @@
+function initialV = spring(K, S1, S2, m)
+initialV = sqrt((5*K*(S2^2-S1^2))/(7*m));
+
+
+
+
