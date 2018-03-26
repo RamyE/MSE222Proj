c31692d44448fcfd71d6b56be95edf1b1035becc
Cao Thanh Nhat Tan <tanpro260196@live.com>
2018-03-25 19:05:28 -0700

    uh, test impact stuff.

--- Impact.m
+++ Impact.m
@@ -1,4 +1,4 @@
-function [matrix]= Impact(angle,time,X,Y,v,c)
+function [matrix]= Impact(angle,time,X,Y,v,global_time,c)
 matrix = [X,Y,0];
 x0=X;
 y0=Y;
@@ -7,7 +7,7 @@ y=Y;
 angle=angle*(pi./180);
 velocity=v*c;
 g=9.81;
-secondtime = 0;
+localtime = 0;
 timejump = 0.001;
 t= 0.001;
 while t < time
@@ -16,14 +16,14 @@ while t < time
     vertical_proj_distance = y0 + velocity*sin((pi/2)-2*angle)*landingtime - 0.5*g*landingtime^2; %Distance in Y-axis of each bounce
     
     if (x <= horizontal_proj_distance) && (x <= 100)
-        secondtime = secondtime + timejump; %Local time of each bounce. t is the global time.
-        x = velocity*cos((pi/2)-2*angle)*secondtime + x0; %X Location equation
-        y = y0 + velocity*sin((pi/2)-2*angle)*secondtime - 0.5*g*secondtime^2; %Y Location equation
-        matrix = [matrix; x y t]; %Output.
+        localtime = localtime + timejump; %Local time of each bounce. t is the temp time (only use inside this function).
+        x = velocity*cos((pi/2)-2*angle)*localtime + x0; %X Location equation
+        y = y0 + velocity*sin((pi/2)-2*angle)*localtime - 0.5*g*localtime^2; %Y Location equation
+        matrix = [matrix; x y (global_time+t)]; %Output.
         t= t + timejump;
     elseif (x > horizontal_proj_distance) && (y< vertical_proj_distance) %This check whether or not a bounce is completed and move to a new bounce.
         t = t - timejump;
-        secondtime=0;
+        localtime=0;
         x0=x;
         y0=y;
         velocity=velocity*c;
--- MainProgram.m
+++ MainProgram.m
@@ -1,6 +1,7 @@
 clc
 clear all;
 close all;
+clearvars;
 
 %This is a list of constants that is dependent on the design (START)
 % I am assuming standard metric units (metre, kg, second, N, etc...)
@@ -26,9 +27,26 @@ tempX = [3,6,7,8,9];
 tempY = [45, 45,  67, 78, 66];
 tempT = [0.001,0.002,0.003,0.004,0.005];
 tempXYT = [tempX',tempY',tempT'];
-GlobalXYT = [GlobalXYT; tempXYT]
+GlobalXYT = [GlobalXYT; tempXYT];
 
 %determine the initial velocity after the spring
 initialV = spring (springK, springS1, springS2, mass);
 %the initial velocity in the X direction.
 %We will start our initial X after the spring distance
+
+
+
+
+%Impact Part
+%The initial conditions below is just asumption.
+angle_impact_slope = 30; %degrees
+global_time = 5; %When the impact start
+bouncing_time = 3; %seconds
+X_after_proj = 50;
+Y_after_proj = 80;
+Contact_velocity = 10; %m/s
+%Coefficient of restitution is the last input. Don't know how to calculate
+%it so I assumed it to be 0.63, which is the typical value of stainless
+%steel.
+[matrix] = Impact(angle_impact_slope,bouncing_time, X_after_proj,Y_after_proj,Contact_velocity, global_time,0.63); %Output is in a 3 matrix-columns matrix (x,y,t). Can add into a global array later.
+GlobalXYT = [GlobalXYT; matrix];
