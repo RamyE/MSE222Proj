5a5b2fc3118f231aad5b030a9766fb11c2b3545a
Cao Thanh Nhat Tan <tanpro260196@live.com>
2018-03-26 23:37:45 -0700

    .............

--- Impact.m
+++ Impact.m
@@ -1,5 +1,7 @@
 function [matrix]= Impact(angle,time,X,Y,v,global_time,c)
-matrix = [X,Y,0];
+%Bounce functions are similiar to projtile function with decreasing
+%velocity after each bounce.
+matrix = [X,Y,global_time];
 x0=X;
 y0=Y;
 x=X;
@@ -8,9 +10,9 @@ angle=angle*(pi./180);
 velocity=v*c;
 g=9.81;
 localtime = 0;
-timejump = 0.001;
+timejump = 0.001; %Time step size is 1ms
 t= 0.001;
-while t < time
+while t < time %The period the ball will bounce, estimated by experiment.
     landingtime = (2*velocity*sin(pi/2 - angle))/(g*cos(angle)); %Total time to complete 1 bounce
     horizontal_proj_distance = ((2*velocity^2*sin(2*angle))/g) + x0; %Distance in X-axis of each bounce
     vertical_proj_distance = y0 + velocity*sin((pi/2)-2*angle)*landingtime - 0.5*g*landingtime^2; %Distance in Y-axis of each bounce
@@ -18,7 +20,7 @@ while t < time
     if (x <= horizontal_proj_distance) && (x <= 100)
         localtime = localtime + timejump; %Local time of each bounce. t is the temp time (only use inside this function).
         x = velocity*cos((pi/2)-2*angle)*localtime + x0; %X Location equation
-        y = y0 + velocity*sin((pi/2)-2*angle)*localtime - 0.5*g*localtime^2; %Y Location equation
+        y = velocity*sin((pi/2)-2*angle)*localtime - 0.5*g*localtime^2 + y0; %Y Location equation
         matrix = [matrix; x y (global_time+t)]; %Output.
         t= t + timejump;
     elseif (x > horizontal_proj_distance) && (y< vertical_proj_distance) %This check whether or not a bounce is completed and move to a new bounce.
