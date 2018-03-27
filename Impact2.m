3fa89ec6d0f30d6d453fd4419cdfd11cdc2932cd
Cao Thanh Nhat Tan <tanpro260196@live.com>
2018-03-27 00:50:36 -0700

    Added the proper way to calculate contact velocity
    As the velocity calculated from the curved part is abnormally large. I will use dummy contact velocity value for the time being.

--- Impact2.m
+++ Impact2.m
@@ -1,14 +1,18 @@
-function [matrix]= Impact2(angle, time, X, Y, v, global_time, c)
+function [matrix]= Impact2(angle, time, X, Y, global_time, c)
 %Bounce functions are similiar to projtile function with decreasing
 %velocity after each bounce.
 %Time: The total time the ball takes to bounce in seconds
 matrix = [X,Y,global_time];
+global GlobalXYT;
+Vx = (GlobalXYT(end,1) - GlobalXYT(end-1,1))/(GlobalXYT(end,3) - GlobalXYT(end-1,3))
+Vy = (GlobalXYT(end,2) - GlobalXYT(end-1,2))/(GlobalXYT(end,3) - GlobalXYT(end-1,3))
+velocity = sqrt(Vx.^2 + Vy.^2) %calculating the initial velocity from the last element data
 x0=X;
 y0=Y;
 x=X;
 y=Y;
 angle=angle*(pi./180);
-velocity=v*c;
+velocity=14*c; %As the velocity calculated from the curved part is abnormally large. I will use this dummy value for the time being.
 g=9.81;
 localtime = 0;
 timejump = 0.001; %Time step size is 1 ms
@@ -27,7 +31,7 @@ while (t <= time)
         y0=y;
         velocity=velocity*c;
     end
-    if round(velocity, 11) == 0
+    if round(velocity, 5) == 0
         break; %Exit the function if velocity become too small to return accurate result.
     end
 end
