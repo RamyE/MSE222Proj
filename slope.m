2a6eb589ac666210059d268b27fa8c1d8756572b
Unknown <jpacamarra@outlook.com>
2018-03-27 08:51:43 -0700

    Revert "Renamed Projectile2 to Projectile"
    This reverts commit 54feed678fb29d3aa57b5581c884e746b73c58d1.

--- slope.m
+++ slope.m
@@ -1,4 +1,3 @@
-<<<<<<< refs/remotes/origin/master
 function [slopeResults] = slope(a,distance)
 % Uses Newtons - Kinematics and Kinetic
 %calc v0
@@ -34,25 +33,4 @@ time=GlobalXYT(end,3)+t;
 
 slopeResults=[x',y',time'];
 
-=======
-function [x,y] = slope(a,s,time,initial_angVel)
-% Will use Kinetic Energy and Work
-%   s=total distance to roll
-
-global GlobalXYT;
-
-angularVelocity = initial_angVel;
-lengthOfRoll = s;
-dtime=0.001;
-t=0:dtime:time;
-x0=GlobalXYT(end,1);
-y0=GlobalXYT(end,2);
-g = 9.81;
-radiusBall=0.05;
-angle=a*(pi./180);
-omega = sqrt( angularVelocity^2 +(g/radiusBall^2)*lengthOfRoll*sin(angle) );
-
-
-
->>>>>>> Renamed Projectile2 to Projectile
 end
