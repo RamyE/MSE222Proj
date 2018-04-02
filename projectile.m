11f0e432b68c11056bcb8bafa9a079444939796a
Unknown <jpacamarra@outlook.com>
2018-04-02 00:01:13 -0700

    Updated the Projectile Function Doc Text

--- projectile.m
+++ projectile.m
@@ -1,22 +1,31 @@
-function [results]= projectile(time)
-% How to use this Function:
-%       This function takes in global time,X,Y 
-%       Use this function if there is a generated X,Y path already
-%       The results are the updated X,Y global coordinates and the
-%       time added to the global time
+function [results]= projectile()
+%    This function only works for our specific dimensions
+%    because it calculates time based on our design
+%    NO INPUT ARGUMENTS NEEDED
 
 global GlobalXYT;
 
 dtime = 0.001; %1 millisecond
-dposx=GlobalXYT(end,1)-GlobalXYT(end-1,1);
-dposy=GlobalXYT(end,2)-GlobalXYT(end-1,2);
-velx=dposx/dtime;
-vely=dposy/dtime;
+initial_dtime = GlobalXYT(end,3)-GlobalXYT(end-1,3);
+x0=GlobalXYT(end,1);
+y0=GlobalXYT(end,2);
+dposx=x0-GlobalXYT(end-1,1);
+dposy=y0-GlobalXYT(end-1,2);
+velx=dposx/initial_dtime;
+vely=dposy/initial_dtime;
 g=9.81;
-t=0:dtime:time;
+
+%Solving for time
+syms T
+eqn= 0.5*g*T^2+( vely+0.5*velx )*T+( y0-0.39+0.5*x0 )==0;
+solT=solve(eqn,T);
+time=double(solT);
+calulatedTime=time(time>0);
+t=0:dtime:calulatedTime;
+
 time=GlobalXYT(end,3)+t;
-x=GlobalXYT(end,1)+velx*t;
-y=GlobalXYT(end,2)+vely*t-(g*t.^2)/2;
+x=x0+velx*t;
+y=y0+vely*t-(g*t.^2)/2;
 
 results = [x',y',time'];
 
