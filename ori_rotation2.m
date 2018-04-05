f9fe8174cb33f4ae2167d06f265dea441ab958db
Ramy <ramyelmallah@gmail.com>
2018-04-05 15:27:26 -0700

    rotation 2

--- ori_rotation2.m
+++ ori_rotation2.m
@@ -1,4 +1,4 @@
-function [result3] = ori_rotation(mb,mr,rb,l)
+function [result3] = ori_rotation2(mb,mr,rb,l)
 global GlobalXYT;
 %mb: ball mass
 %mr: rod mass
@@ -7,7 +7,7 @@ global GlobalXYT;
 %d: horizontal bar length
 %v1: ball initial velocity
 g = 9.81;
-e = 0.01; %coefficient of restitution
+e = 0.10; %coefficient of restitution
 x0 = GlobalXYT(end,1);
 y0 = GlobalXYT(end,2);
 % x0 = 0.251;
@@ -32,12 +32,22 @@ y1 = (mr*(l/2) + 0*(l-rb))/(mr + 0);
 cos_of_angle = (1-(0.5*(mb*(v2^2)+I*(wr2^2)))/((0+mr)*g*y1));
 angle = acos(cos_of_angle);
 
-theta = 0:0.001:angle;
-syms OM
-eqn = [0.5*mb*((v2^2))+(0.5*Ir*(wr2^2))-mr*y1*g == -mr*y1*g*cos(theta)+ (0.5*mb*((l-rb)*(OM.^2))+(0.5*Ir*(OM.^2)))];
-[omega] = solve(eqn, [OM])
+theta = 0:0.01:angle;
+omegaF =[];
+tF = 0;
+for i = 2:length(theta)
+    syms OM;
+    eqn = [0.5*mb*((v2^2))+(0.5*Ir*(wr2^2))-mr*y1*g == (-mr*y1*g*cos(theta(i))+ 0.5*mb*((l-rb)*(OM.^2))+(0.5*Ir*(OM.^2)))];
+    omega = solve(eqn, OM);
+    omegad = double(omega);
+    omegad = omegad(omegad>0);
+    timee = (theta(i)-theta(i-1)) / omegad;
+    omegaF = [omegaF; omegad];
+    tF = [tF, tF(end)+timee];
+end
 
-% t = theta./wr2;
+
+t = theta ./ omegaF;
 % wr2 = 0 -;
 % alpha = wr2./t;
 
@@ -45,13 +55,14 @@ eqn = [0.5*mb*((v2^2))+(0.5*Ir*(wr2^2))-mr*y1*g == -mr*y1*g*cos(theta)+ (0.5*mb*
 % x = x0 + v2.*t + 0.5.*ax.*(t.^2);
 % ay = (l-rb).*alpha.*sin(theta/2); %ay: vertical acceleration after impact
 % y = y0 + 0.5*(-ay-g).*(t.^2);
-x = x0 + l.*sin(theta);
-y = y0 + 2*l.*(sin(theta/2)).^2;
-time = GlobalXYT(end,3)+ t;
 
+% x = x0 + l.*sin(theta);
+% y = y0 + 2*l.*(sin(theta/2)).^2;
+% time = GlobalXYT(end,3)+ t;
+% 
 result3 = [x' y' time'];
-
-hold on
+% 
+% hold on
 % figure;
 % subplot(2,1,1)
 % plot(x,y,'r')
@@ -60,9 +71,9 @@ hold on
 % title('Ball position in Rotation');
 % 
 % subplot(2,1,2)
-plot(t,wr2,'r')
-xlabel('X');
-ylabel('Y');
-title('Angular Velocity in Rotation');
+% plot(t,wr2,'r')
+% xlabel('X');
+% ylabel('Y');
+% title('Angular Velocity in Rotation');
 
 end
